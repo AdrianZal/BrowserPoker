37a09900c0e100efd975684d3177c5ff48031b3c
Aracrooz <aracrooz@gmail.com>
2025-09-07 16:01:14 +0200

    Poker engine v1.0

---
+++ Poker/Poker/Program.cs
@@ -0,0 +1,9 @@
+﻿using Poker;
+
+Table table = new Table(1000);
+table.AddPlayer(new Player("Marek", table.buyIn));
+table.AddPlayer(new Player("Szparek", table.buyIn));
+table.AddPlayer(new Player("Darek", table.buyIn));
+table.PlayRound();
+table.PlayRound();
+table.PlayRound();
